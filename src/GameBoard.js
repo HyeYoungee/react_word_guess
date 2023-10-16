64b7c9d5e1ff99de1199800b6f01b2aad8a671f2
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-10-16 12:46:43 +0900

    231016 [Add] button 클릭 매칭 기능 추가

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import ButtonGrid from "./ButtonGrid";
 import LetterGrid from "./LetterGrid";
 
 const GameBoard = ({secretWord}) => {
-  const letterGuessedHandler = () => {
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  const letterGuessedHandler = (letter) => {
+    let val = letter.toLowerCase();
+
+    setGuessedLetters(prev => [...prev, val]);
+
 
   }
 
   return(
     <>
-      <LetterGrid secretWord={secretWord} guessedLetters={['r', 'e']}/>
+      <LetterGrid secretWord={secretWord} guessedLetters={guessedLetters}/>
       <ButtonGrid letterGuessed={letterGuessedHandler}/>
     </>
   )
